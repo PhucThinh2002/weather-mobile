62296a4184df3f75bf99fc0eb361a7e183acdd55
thinh <duongphucthinh300702@gmail.com>
2025-09-25 14:37:32 +0700

    update api moi

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.weatherapi.com/v1';
-const API_KEY = '1f6ced3bfab143cda1e44028240205'; // Thay API key của bạn vào đây
+const API_KEY = '90455b56f40a466594c33750251804'; // Thay API key của bạn vào đây
 
 const api = axios.create({
   baseURL: BASE_URL,
